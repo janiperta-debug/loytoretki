cd385bba03ddd6b776f1c81caa0b4c689aaa8065
janiperta-debug <jani.perta@gmail.com>
2026-09-10 15:55:34 +0300

    Clarify public log wording

--- app/sovellus/page.tsx
+++ app/sovellus/page.tsx
@@ -19,7 +19,7 @@ export default function KotiPage() {
         <ObservationForm />
         <Link href="/sovellus/paivakirja" className="block">
           <InfoCard title="Retkeilijän loki" icon={<NotebookPen className="h-5 w-5 text-brass" aria-hidden />} action="Avaa loki">
-            <p>Katso tuoreita havaintoja muilta retkeilijöiltä ja jaa omasi. Lokin lukeminen ja havaintojen tekeminen onnistuu ilman tiliä.</p>
+            <p>Katso retkeilijöiden tarinoita ja löytöjä. Lokin lukeminen onnistuu ilman tiliä; oman merkinnän voi julkaista kirjautuneena.</p>
           </InfoCard>
         </Link>
         <InfoCard title="Mukana useita lähteitä" icon={<Store className="h-5 w-5 text-brass" aria-hidden />}>
